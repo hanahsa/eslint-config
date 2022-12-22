344a254234438ffc44cd14ff4c77fd6ab3e4f4d8
padorang684 <padorang684@gmail.com>
2022-12-22 12:08:21 +1100

    fix

--- base_config.js
+++ base_config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
+    languageOptions: {
+        parser: '@typescript-eslint/parser',
+    },
     plugins: [
       '@typescript-eslint',
       'import',
